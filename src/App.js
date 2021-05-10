148599726ee8a7a29fa1450b987e7df016d1f0b7
MGubernick <MGubernick61@gmail.com>
2021-05-10 09:22:53 -0500

    Second Commit
    - Installed dependancies

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, Fragment } from 'react';
+// import { Route } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      user: null
+    };
+  }
+  render () {
+    return (
+      <Fragment>
+        <main>
+          <h1>This is where the content goes</h1>
+        </main>
+      </Fragment>
+    );
+  }
+  
 }
 
 export default App;
